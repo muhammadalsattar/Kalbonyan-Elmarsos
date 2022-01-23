6fc1a2c64afd95180de6aacd586d77916a27f4b3
Muhammad Abd-Elsattar <62842603+muhammadalsattar@users.noreply.github.com>
2022-01-23 12:35:02 +0200

    Update functions-101.js

--- Udemy/The Modern JavaScript Bootcamp/S4 JavaScript Functions/functions-101.js
+++ Udemy/The Modern JavaScript Bootcamp/S4 JavaScript Functions/functions-101.js
@@ -1,1 +1,10 @@
-The Modern JavaScript Bootcamp
+let convertFahrenheitToCelsius = function (fahrenheit) {
+    let celsius = (fahrenheit - 32) * 5 / 9
+    return celsius
+}
+
+let tempOne = convertFahrenheitToCelsius(32)
+let tempTwo = convertFahrenheitToCelsius(68)
+
+console.log(tempOne)
+console.log(tempTwo)
