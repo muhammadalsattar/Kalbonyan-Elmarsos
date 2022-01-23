32d225c9afbb3f2ad7178433934f6522e1a16a33
Muhammad Abd-Elsattar <62842603+muhammadalsattar@users.noreply.github.com>
2022-01-23 09:46:59 +0200

    Update Guessing-Game-Challenge.py

--- Linkedin-Learning/Programming-Foundation-Fundamentals/Challengs/Guessing-Game-Challenge.py
+++ Linkedin-Learning/Programming-Foundation-Fundamentals/Challengs/Guessing-Game-Challenge.py
@@ -1,1 +1,14 @@
+# setting food name
+favourite_food = "pizza"
 
+# assigning user guess to a variable
+guess = input("What's my favourite food?")
+
+# checking for result with if else conditional statements
+if guess == favourite_food:
+    print("Yup! so amazing!")
+else:
+    print("Yuck! that's not it!")
+
+# thanking user for playing whatever they input
+print("Thanks for playing!")
