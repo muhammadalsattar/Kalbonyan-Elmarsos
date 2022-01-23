e80266dd6118a83ca9c75b428be1aa4fc55e2125
Muhammad Abd-Elsattar <62842603+muhammadalsattar@users.noreply.github.com>
2022-01-23 12:34:13 +0200

    Update arguments.js

--- Udemy/The Modern JavaScript Bootcamp/S4 JavaScript Functions/arguments.js
+++ Udemy/The Modern JavaScript Bootcamp/S4 JavaScript Functions/arguments.js
@@ -1,1 +1,8 @@
-The Modern JavaScript Bootcamp
+let getTip = function (total, tipPercent = .2) {
+    let percent = tipPercent * 100
+    let tip = total * tipPercent
+    return `A ${percent}% tip on $${total} would be $${tip}`
+}
+
+let tip = getTip(60)
+console.log(tip)
