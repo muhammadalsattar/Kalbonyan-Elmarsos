2e05f5ed76678aa2866226d4beb53608456053e3
Muhammad Abd-Elsattar <62842603+muhammadalsattar@users.noreply.github.com>
2022-01-23 12:39:54 +0200

    Update string-methods.js

--- Udemy/The Modern JavaScript Bootcamp/S5 JavaScript Objects/string-methods.js
+++ Udemy/The Modern JavaScript Bootcamp/S5 JavaScript Objects/string-methods.js
@@ -1,1 +1,8 @@
-The Modern JavaScript Bootcamp
+// isValidPassword
+let isValidPassword = function (password) {
+    return password.length > 8 && !password.includes('password')
+}
+
+console.log(isValidPassword('asdfp'))
+console.log(isValidPassword('abc123!@#$%^&'))
+console.log(isValidPassword('asdfpasdfpoijpassword'))
