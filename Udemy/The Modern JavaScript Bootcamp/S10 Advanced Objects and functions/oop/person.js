c6539d7e728de99d8ce71e99f1cfcbcb4b537b2c
Muhammad Abd-Elsattar <62842603+muhammadalsattar@users.noreply.github.com>
2022-01-23 10:41:01 +0200

    Update person.js

--- Udemy/The Modern JavaScript Bootcamp/S10 Advanced Objects and functions/oop/person.js
+++ Udemy/The Modern JavaScript Bootcamp/S10 Advanced Objects and functions/oop/person.js
@@ -1,1 +1,58 @@
-The Modern JavaScript Bootcamp
+// myPerson --> Person.prototype --> Object.prototype --> null
+
+class Person {
+    constructor(firstName, lastName, age, likes = []) {
+        this.firstName = firstName
+        this.lastName = lastName
+        this.age = age
+        this.likes = likes
+    }
+    getBio() {
+        let bio = `${this.firstName} is ${this.age}.`
+
+        this.likes.forEach((like) => {
+            bio += ` ${this.firstName} likes ${like}.`
+        })
+
+        return bio
+    }
+    set fullName(fullName) {
+        const names = fullName.split(' ')
+        this.firstName = names[0]
+        this.lastName = names[1]
+    }
+    get fullName() {
+        return `${this.firstName} ${this.lastName}`
+    }
+}
+
+class Employee extends Person {
+    constructor(firstName, lastName, age, position, likes) {
+        super(firstName, lastName, age, likes)
+        this.position = position
+    }
+    getBio() {
+        return `${this.fullName} is a ${this.position}.`
+    }
+    getYearsLeft() {
+        return 65 - this.age
+    }
+}
+
+class Student extends Person {
+    constructor(firstName, lastName, age, grade, likes) {
+        super(firstName, lastName, age, likes)
+        this.grade = grade
+    }
+    updateGrade(change) {
+        this.grade += change
+    }
+    getBio() {
+        const status = this.grade >= 70 ? 'passing' : 'failing'
+        return `${this.firstName} is ${status} the class.`
+    }
+}
+
+const me = new Employee('Muhammad', 'Abd-Elsattar', 22, 'Software developer', [])
+me.fullName = 'Hassan Saleh'
+console.log(me.getBio())
