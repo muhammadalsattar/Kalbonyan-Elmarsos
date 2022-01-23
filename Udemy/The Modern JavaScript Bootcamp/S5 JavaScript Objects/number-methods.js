c0f593565b813f7908fb2285e168efab61eee8c2
Muhammad Abd-Elsattar <62842603+muhammadalsattar@users.noreply.github.com>
2022-01-23 12:37:09 +0200

    Update number-methods.js

--- Udemy/The Modern JavaScript Bootcamp/S5 JavaScript Objects/number-methods.js
+++ Udemy/The Modern JavaScript Bootcamp/S5 JavaScript Objects/number-methods.js
@@ -1,1 +1,10 @@
-The Modern JavaScript Bootcamp
+// 1 - 5 - true if correct - false if not correct
+let makeGuess = function (guess) {
+    let min = 1
+    let max = 5
+    let randomNum = Math.floor(Math.random() * (max - min + 1)) + min
+
+    return guess === randomNum
+}
+
+console.log(makeGuess(1))
