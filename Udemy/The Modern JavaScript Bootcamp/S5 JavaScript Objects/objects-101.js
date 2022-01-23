5accb1417e27b7f28d71f1f5614972daa50fce23
Muhammad Abd-Elsattar <62842603+muhammadalsattar@users.noreply.github.com>
2022-01-23 12:38:01 +0200

    Update objects-101.js

--- Udemy/The Modern JavaScript Bootcamp/S5 JavaScript Objects/objects-101.js
+++ Udemy/The Modern JavaScript Bootcamp/S5 JavaScript Objects/objects-101.js
@@ -1,1 +1,23 @@
-The Modern JavaScript Bootcamp
+let myBook = {
+    title: '1984',
+    author: 'George Orwell',
+    pageCount: 326
+}
+
+console.log(`${myBook.title} by ${myBook.author}`)
+
+myBook.title = 'Animal Farm'
+
+console.log(`${myBook.title} by ${myBook.author}`)
+
+// Challenge area
+
+let me = {
+    name: 'Muhammad',
+    age: 27,
+    location: 'Alexandria'
+}
+
+console.log(`${me.name} is ${me.age} and lives in ${me.location}.`)
+me.age = me.age + 1
+console.log(`${me.name} is ${me.age} and lives in ${me.location}.`)
