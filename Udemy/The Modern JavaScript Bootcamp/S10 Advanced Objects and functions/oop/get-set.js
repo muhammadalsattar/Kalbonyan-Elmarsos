32efa3cff205f4009de87fd8a277263057fab484
Muhammad Abd-Elsattar <62842603+muhammadalsattar@users.noreply.github.com>
2022-01-23 10:36:54 +0200

    Update get-set.js

--- Udemy/The Modern JavaScript Bootcamp/S10 Advanced Objects and functions/oop/get-set.js
+++ Udemy/The Modern JavaScript Bootcamp/S10 Advanced Objects and functions/oop/get-set.js
@@ -1,1 +1,16 @@
-The Modern JavaScript Bootcamp
+// Setting up object
+const data = {
+    locations: [],
+    get location() {
+        return this._location
+    },
+    set location(value) {
+        this._location = value.trim()
+        this.locations.push(this._location)
+    }
+}
+
+// Setting data location
+data.location = '  Philadelphia    '
+data.location = ' New York'
+console.log(data)
