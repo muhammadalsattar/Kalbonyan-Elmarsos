c6676a156cb6ef4dafa86241889a6179efa08931
Muhammad Abd-Elsattar <62842603+muhammadalsattar@users.noreply.github.com>
2022-01-23 11:20:08 +0200

    Update boolean.js

--- Udemy/The Modern JavaScript Bootcamp/S3 JavaScript Basics Variables and Flow Control/boolean.js
+++ Udemy/The Modern JavaScript Bootcamp/S3 JavaScript Basics Variables and Flow Control/boolean.js
@@ -1,1 +1,11 @@
-The Modern JavaScript Bootcamp
+let age = 10
+
+// If 7 or under print message about child pricing
+if (age <= 7) {
+    console.log('You will get a child discount!')
+}
+
+// If 65 or older print message about senior discount
+if (age >= 65) {
+    console.log('You will get a senior discount!')
+}
