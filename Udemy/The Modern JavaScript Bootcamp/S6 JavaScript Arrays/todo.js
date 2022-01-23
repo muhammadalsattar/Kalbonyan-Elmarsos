39ab002ce838fa6e4d89c6a8747a7c13c51b3138
Muhammad Abd-Elsattar <62842603+muhammadalsattar@users.noreply.github.com>
2022-01-23 12:41:12 +0200

    Update todo.js

--- Udemy/The Modern JavaScript Bootcamp/S6 JavaScript Arrays/todo.js
+++ Udemy/The Modern JavaScript Bootcamp/S6 JavaScript Arrays/todo.js
@@ -1,1 +1,53 @@
-The Modern JavaScript Bootcamp
+const todos = [{
+    text: 'Order cat food',
+    completed: false
+}, {
+    text: 'Clean kitchen',
+    completed: true
+}, {
+    text: 'Buy food',
+    completed: true
+}, {
+    text: 'Do work',
+    completed: false
+}, {
+    text: 'Exercise',
+    completed: true
+}]
+// -1 means that a would come before b
+// 1 means that b would come before a
+const sortTodos = function (todos) {
+    todos.sort(function (a, b) {
+        if (!a.completed && b.completed) {
+            return -1
+        } else if (!b.completed && a.completed) {
+            return 1
+        } else {
+            return 0
+        }
+    })
+}
+
+const deleteTodo = function (todos, todoText) {
+    const index = todos.findIndex(function (todo) {
+        return todo.text.toLowerCase() === todoText.toLowerCase()
+    })
+
+    if (index > -1) {
+        todos.splice(index, 1)
+    }
+}
+
+const getThingsToDo = function (todos) {
+    return todos.filter(function (todo) {
+        return !todo.completed
+    })
+}
+
+sortTodos(todos)
+console.log(todos)
+
+// console.log(getThingsToDo(todos))
+ 
+// deleteTodo(todos, '!!buy food')
+// console.log(todos)
