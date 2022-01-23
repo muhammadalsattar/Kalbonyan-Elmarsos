326747cbbb2a8dbe645fac227a4c4b7f0a1b2219
Muhammad Abd-Elsattar <62842603+muhammadalsattar@users.noreply.github.com>
2022-01-23 12:30:37 +0200

    Update scope-part2.js

--- Udemy/The Modern JavaScript Bootcamp/S3 JavaScript Basics Variables and Flow Control/scope-part2.js
+++ Udemy/The Modern JavaScript Bootcamp/S3 JavaScript Basics Variables and Flow Control/scope-part2.js
@@ -1,1 +1,19 @@
-The Modern JavaScript Bootcamp
+// Global
+  // Local
+    // Local (name)
+  // Local
+
+///let name = 'Muhammad'
+
+if (true) {
+    //let name = 'Mike'
+
+    if (true) {
+        let name = 'Jen'
+        console.log(name)
+    }
+}
+
+if (true) {
+    console.log(name)
+}
