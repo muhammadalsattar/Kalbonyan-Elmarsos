ab82e1481015391d69ae7e70fc5aa90ef76aaadc
Muhammad Abd-Elsattar <62842603+muhammadalsattar@users.noreply.github.com>
2022-01-23 12:32:20 +0200

    Update temp-conversion.js

--- Udemy/The Modern JavaScript Bootcamp/S3 JavaScript Basics Variables and Flow Control/temp-conversion.js
+++ Udemy/The Modern JavaScript Bootcamp/S3 JavaScript Basics Variables and Flow Control/temp-conversion.js
@@ -1,1 +1,6 @@
-The Modern JavaScript Bootcamp
+let fahrenheit = 70 // 0c - 273.15k
+let celsius = (fahrenheit - 32) * 5 / 9
+let kelvin = (fahrenheit + 459.67) * 5 / 9
+
+console.log('Tempeature in celsius: ' + celsius)
+console.log('Tempeature in kelvin: ' + kelvin)
